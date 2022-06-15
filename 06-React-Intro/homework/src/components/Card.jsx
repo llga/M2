fcaffe9a3b28570c2b3ba0bc0e466948d9d024a0
Jose Lopez <josefrancisco2697@gmail.com>
2022-06-15 07:16:29 -0500

    Homework React Intro

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
 
-export default function Card(props) {
+export default function Card({ max, min, name, img, onClose }) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div>
+      <button onClick={onClose}>X</button>
+      <h3>{name}</h3>
+      <div>
+        <div>
+          <p>Min</p>
+          <span>{min}</span>
+        </div>
+        <div>
+          <p>Max</p>
+          <span>{max}</span>
+        </div>
+        <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt={`clima de ${name} `}/>
+      </div>
+    </div>
+  )
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-export default function SearchBar(props) {
+export default function SearchBar({onSearch}) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  const buscar = () => {
+    const inputSearch = document.querySelector("#inputSearch")
+    onSearch(inputSearch.value)
+    inputSearch.focus()
+  }
+  return <div>
+    <input id='inputSearch' type="text" placeholder="Buscar por ciudad..." />
+    <button onClick={buscar}>Buscar</button>
+  </div>
 };
